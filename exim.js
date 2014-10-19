00c862478e9481ca1f851d2050331138b579a445
Artem Yavorsky <aqson@me.com>
2014-10-20 00:39:06 +0300

    Update main

--- exim.js
+++ exim.js
@@ -89,246 +89,6 @@ utils.bindTags = function (tags, scope) {
     scope[tag] = React.DOM[tag];
   })
 };
-<<<<<<< HEAD:exim.js
-// Copyright (c) 2014 GitHub, Inc.
-
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-(function() {
-  'use strict';
-
-  if (window.fetch) {
-    return
-  }
-
-  function Headers(headers) {
-    this.map = {}
-
-    var self = this
-    if (headers instanceof Headers) {
-      headers.forEach(function(name, values) {
-        values.forEach(function(value) {
-          self.append(name, value)
-        })
-      })
-
-    } else if (headers) {
-      Object.getOwnPropertyNames(headers).forEach(function(name) {
-        self.append(name, headers[name])
-      })
-    }
-  }
-
-  Headers.prototype.append = function(name, value) {
-    var list = this.map[name]
-    if (!list) {
-      list = []
-      this.map[name] = list
-    }
-    list.push(value)
-  }
-
-  Headers.prototype['delete'] = function(name) {
-    delete this.map[name]
-  }
-
-  Headers.prototype.get = function(name) {
-    var values = this.map[name]
-    return values ? values[0] : null
-  }
-
-  Headers.prototype.getAll = function(name) {
-    return this.map[name] || []
-  }
-
-  Headers.prototype.has = function(name) {
-    return this.map.hasOwnProperty(name)
-  }
-
-  Headers.prototype.set = function(name, value) {
-    this.map[name] = [value]
-  }
-
-  // Instead of iterable for now.
-  Headers.prototype.forEach = function(callback) {
-    var self = this
-    Object.getOwnPropertyNames(this.map).forEach(function(name) {
-      callback(name, self.map[name])
-    })
-  }
-
-  function consumed(body) {
-    if (body.bodyUsed) {
-      return Promise.reject(new TypeError('Body already consumed'))
-    }
-    body.bodyUsed = true
-  }
-
-  function Body() {
-    this.body = null
-    this.bodyUsed = false
-
-    this.arrayBuffer = function() {
-      throw new Error('Not implemented yet')
-    }
-
-    this.blob = function() {
-      var rejected = consumed(this)
-      return rejected ? rejected : Promise.resolve(new Blob([this.body]))
-    }
-
-    this.formData = function() {
-      return Promise.resolve(decode(this.body))
-    }
-
-    this.json = function() {
-      var rejected = consumed(this)
-      if (rejected) {
-        return rejected
-      }
-
-      var body = this.body
-      return new Promise(function(resolve, reject) {
-        try {
-          resolve(JSON.parse(body))
-        } catch (ex) {
-          reject(ex)
-        }
-      })
-    }
-
-    this.text = function() {
-      var rejected = consumed(this)
-      return rejected ? rejected : Promise.resolve(this.body)
-    }
-
-    return this
-  }
-
-  function Request(url, options) {
-    options = options || {}
-    this.url = url
-    this.body = options.body
-    this.credentials = options.credentials || null
-    this.headers = new Headers(options.headers)
-    this.method = options.method || 'GET'
-    this.mode = options.mode || null
-    this.referrer = null
-  }
-
-  function encode(params) {
-    return Object.getOwnPropertyNames(params).filter(function(name) {
-      return params[name] !== undefined
-    }).map(function(name) {
-      var value = (params[name] === null) ? '' : params[name]
-      return encodeURIComponent(name) + '=' + encodeURIComponent(value)
-    }).join('&').replace(/%20/g, '+')
-  }
-
-  function decode(body) {
-    var form = new FormData()
-    body.trim().split('&').forEach(function(bytes) {
-      if (bytes) {
-        var split = bytes.split('=')
-        var name = split.shift().replace(/\+/g, ' ')
-        var value = split.join('=').replace(/\+/g, ' ')
-        form.append(decodeURIComponent(name), decodeURIComponent(value))
-      }
-    })
-    return form
-  }
-
-  function isObject(value) {
-    try {
-      return Object.getPrototypeOf(value) === Object.prototype
-    } catch (ex) {
-      // Probably a string literal.
-      return false
-    }
-  }
-
-  function headers(xhr) {
-    var head = new Headers()
-    var pairs = xhr.getAllResponseHeaders().trim().split('\n')
-    pairs.forEach(function(header) {
-      var split = header.trim().split(':')
-      var key = split.shift().trim()
-      var value = split.join(':').trim()
-      head.append(key, value)
-    })
-    return head
-  }
-
-  Request.prototype.fetch = function() {
-    var self = this
-
-    return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest()
-
-      xhr.onload = function() {
-        var options = {
-          status: xhr.status,
-          statusText: xhr.statusText,
-          headers: headers(xhr)
-        }
-        resolve(new Response(xhr.responseText, options))
-      }
-
-      xhr.onerror = function() {
-        reject()
-      }
-
-      xhr.open(self.method, self.url)
-
-      self.headers.forEach(function(name, values) {
-        values.forEach(function(value) {
-          xhr.setRequestHeader(name, value)
-        })
-      })
-
-      var body = self.body
-      if (isObject(self.body)) {
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-        body = encode(self.body)
-      }
-      xhr.send(body)
-    })
-  }
-
-  Body.call(Request.prototype)
-
-  function Response(body, options) {
-    this.body = body
-    this.type = 'default'
-    this.url = null
-    this.status = options.status
-    this.statusText = options.statusText
-    this.headers = options.headers
-  }
-
-  Body.call(Response.prototype)
-
-  window.fetch = function (url, options) {
-    return new Request(url, options).fetch()
-  }
-})();
 // Copyright (c) 2014 Ryan Florence, Michael Jackson
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -349,8 +109,6 @@ utils.bindTags = function (tags, scope) {
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-=======
->>>>>>> 46147e9d4f820905c847a9982886d918cfbc362c:toothpaste.js
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.ReactRouter=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var LocationDispatcher = _dereq_('../dispatchers/LocationDispatcher');
 var makePath = _dereq_('../utils/makePath');
@@ -5508,17 +5266,6 @@ Fluxy.prototype = utils.extend(Fluxy.prototype, {
   for (var tagName in React.DOM) {
     bindTag(tagName);
   }
-<<<<<<< HEAD:exim.js
-=======
-
-  domHelpers['space'] = function() {
-    return React.DOM.span({
-      dangerouslySetInnerHTML: {
-        __html: '&nbsp;'
-      }
-    });
-  };
->>>>>>> 46147e9d4f820905c847a9982886d918cfbc362c:toothpaste.js
 
   domHelpers['space'] = function() {
     return React.DOM.span({
@@ -5531,16 +5278,8 @@ Fluxy.prototype = utils.extend(Fluxy.prototype, {
   Exim.DOM = domHelpers;
 
   Exim.addTag = function (name, tag) {
-    this.DOM[name] = tag
+    this.DOM[name] = tag;
   }
 
-<<<<<<< HEAD:exim.js
   return Exim;
-=======
-  Toothpaste.addTag = function (name, tag) {
-    this.DOM[name] = tag
-  }
-
-  return Toothpaste;
->>>>>>> 46147e9d4f820905c847a9982886d918cfbc362c:toothpaste.js
 });
