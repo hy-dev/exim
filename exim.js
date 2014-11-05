c3d20009fe7174a29560c6ede99b4da3f09931b2
Artem Yavorsky <aqson@me.com>
2014-11-05 22:09:25 +0200

    Make exim

--- exim.js
+++ exim.js
@@ -5829,6 +5829,45 @@ Exim.DOM = domHelpers;
 Exim.addTag = function (name, tag) {
   this.DOM[name] = tag;
 }
-  return Exim;
 
+  var toS = Object.prototype.toString;
+
+  Exim.Router = {
+    match: function(name, View) {
+      var options = {};
+
+      var routes = [].slice.call(arguments, 2);
+      if (typeof name !== 'string') {
+        routes = [View].concat(routes);
+        View = name;
+        name = null;
+      }
+
+      var firstRouteIsOptions = !(routes[0] && 'props' in routes[0]);
+      if (routes[0] && firstRouteIsOptions) {
+        options = routes[0];
+        routes = routes.slice(1);
+      }
+
+      var opts = {handler: View};
+      if (name) opts.name = name;
+      if (options.path) opts.path = options.path;
+      var args = [opts].concat(routes);
+
+      return ReactRouter.Route.apply(ReactRouter.Route, args);
+    },
+    startHistory: function() {
+      var routes = [].slice.call(arguments);
+      return ReactRouter.Routes.apply(ReactRouter.Routes, [{location: 'history'}].concat(routes));
+    },
+    defaultTo: function(View) {
+      return ReactRouter.DefaultRoute({handler: View});
+    },
+    Link: ReactRouter.Link,
+    transitionTo: ReactRouter.transitionTo,
+    goBack: ReactRouter.goBack,
+    replaceWith: ReactRouter.replaceWith
+  };
+
+  return Exim;
 });
