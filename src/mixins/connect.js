64fe890b33ffd1638e0ec0575c9039ff245a3dee
Chris McVittie <chris.mcvittie@blue-zinc.com>
2015-09-11 10:14:01 +0100

    bound callbacks need to be per-component and support multiple mixins per component

--- src/mixins/connect.js
+++ src/mixins/connect.js
@@ -3,24 +3,29 @@ export default function getConnectMixin (store) {
     this.setState(state.toJS());
   };
 
-  let listener, changeCallbackBound;
+  let listener;
 
   return {
     getInitialState: function () {
       const frozen = store.store.get(arguments);
       const state = frozen.toJS();
-      changeCallbackBound = changeCallback.bind(this);
+
+      if (!this.boundEximChangeCallbacks)
+        this.boundEximChangeCallbacks = {};
+
+      this.boundEximChangeCallbacks[store] = changeCallback.bind(this);
+
       listener = frozen.getListener();
       return state;
     },
 
     componentDidMount: function () {
-      listener.on('update', changeCallbackBound);
+      listener.on('update', this.boundEximChangeCallbacks[store]);
     },
 
     componentWillUnmount: function () {
       if (listener)
-        listener.off('update', changeCallbackBound);
+        listener.off('update', this.boundEximChangeCallbacks[store]);
     }
   };
 }
